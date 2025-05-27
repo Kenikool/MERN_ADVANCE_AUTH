88e1e2c337442deaf8ce4bb0e942aa61c7e07239
kenikool <kenikoolchaawa070@gmail.com>
2025-05-27 20:16:07 +0100

    013_signup_implementation

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
@@ -10,10 +11,7 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(bodyParser.json());
 app.use(cookieParser());
-// Define your routes here
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
-});
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 //  routes
 app.use("/api/auth", authRoutes);
--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,19 +1,28 @@
-import React, { useState } from "react";
-import { useNavigate, Link } from "react-router-dom";
 import { motion } from "framer-motion";
 import Input from "../components/Input";
-import { Lock, Mail, User } from "lucide-react";
+import { Loader, Lock, Mail, User } from "lucide-react";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import PasswordStrengthMeter from "../components/PasswordStrenthMeter";
+import { useAuthStore } from "../store/authStore";
+
 const SignUpPage = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
-  const isLoading = false;
-  const error = null;
+
+  const { signup, error, isLoading } = useAuthStore();
+
   const handleSignUp = async (e) => {
     e.preventDefault();
-    console.log({ name, email, password });
+
+    try {
+      await signup(email, password, name);
+      navigate("/verify-email");
+    } catch (error) {
+      console.log(error);
+    }
   };
   return (
     <motion.div
@@ -36,24 +45,23 @@ const SignUpPage = () => {
             value={name}
             onChange={(e) => setName(e.target.value)}
           />
-
           <Input
             icon={Mail}
             type="email"
-            placeholder="johndoe@example.com"
+            placeholder="Email Address"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           />
-
           <Input
             icon={Lock}
-            type="text"
+            type="password"
             placeholder="Password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
           {error && <p className="text-red-500 font-semibold mt-2">{error}</p>}
           <PasswordStrengthMeter password={password} />
+
           <motion.button
             className="mt-5 w-full py-3 px-4 bg-gradient-to-r from-green-500 to-emerald-600 text-white 
 						font-bold rounded-lg shadow-lg hover:from-green-600
@@ -71,17 +79,16 @@ const SignUpPage = () => {
             )}
           </motion.button>
         </form>
-        <div className="px-8 py-4 bg-gray-900 bg-opacity-50 flex justify-center">
-          <p className="text-sm text-gray-400">
-            Already have an account?{" "}
-            <Link to={"/login"} className="text-green-400 hover:underline">
-              Login
-            </Link>
-          </p>
-        </div>
+      </div>
+      <div className="px-8 py-4 bg-gray-900 bg-opacity-50 flex justify-center">
+        <p className="text-sm text-gray-400">
+          Already have an account?{" "}
+          <Link to={"/login"} className="text-green-400 hover:underline">
+            Login
+          </Link>
+        </p>
       </div>
     </motion.div>
   );
 };
-
 export default SignUpPage;
